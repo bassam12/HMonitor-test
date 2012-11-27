8110d554553862b0a65f5fc7a2c5eca8a99fbb45
Anil karri <anil.karrii@gmail.com>
2012-11-27 18:14:09 +0530

    Update src/com/prav/android/mhealth/BluetoothChat.java
    Add webservice clinic2care site

--- src/com/prav/android/mhealth/BluetoothChat.java
+++ src/com/prav/android/mhealth/BluetoothChat.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.prav.android.mhealth;
 
@@ -58,7 +43,7 @@ public class BluetoothChat extends Activity {
 	//Webservices 
 	
 	private final String NAMESPACE = "http://ws.mhealth.prav.com";
-    private final String URL = "http://192.168.10.58:8005/mhealthWS/services/PatientsWs?wsdl";
+    private final String URL = "http://www.clinic2care.com/axis2/services/PatientsWs?wsdl";
     private final String SOAP_ACTION = "http://ws.mhealth.prav.com";
     private final String METHOD_NAME = "insertData";
     TextView hvalue;
